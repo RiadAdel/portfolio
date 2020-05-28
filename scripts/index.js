9d1b90294dcdd5dd25729157ac95fbcf609c710c
Riad Adel <riadadel22@gmail.com>
2020-05-28 15:39:39 +0200

    add previous work

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,71 @@
-function scroll(trigger, content){
-    trigger.addEventListener('click',function(e){
+function scroll(trigger, content) {
+    trigger.addEventListener('click', function (e) {
         console.log(e)
         content.scrollIntoView(false);
     })
 }
 
-function typingAnimation(elm,content,delay = 150,removeAfterFinish = true){
-    let count =0;
+function typingAnimation(elm, content, delay = 150, removeAfterFinish = true) {
+    let count = 0;
     elm.classList.add("cursor")
-    let intervalID = setInterval(function(){
+    let intervalID = setInterval(function () {
         elm.innerText += content[count++];
-        if(count == content.length){
+        if (count == content.length) {
             clearInterval(intervalID)
-            if(removeAfterFinish)
+            if (removeAfterFinish)
                 elm.classList.remove("cursor")
         }
-    },delay)
+    }, delay)
 }
 
-document.addEventListener('DOMContentLoaded',function(){
+function prevSlide(slider, currentSlide) {
+    const slides = slider.querySelectorAll("div.slide");
+    slides[currentSlide].classList.remove("current");
+    currentSlide = currentSlide == 0 ? slides.length - 1 : currentSlide - 1
+    slides[currentSlide].classList.add("current");
+    return currentSlide
+}
+
+function nextSlide(slider, currentSlide) {
+    const slides = slider.querySelectorAll("div.slide");
+    slides[currentSlide].classList.remove("current");
+    currentSlide = currentSlide == slides.length - 1 ? 0 : currentSlide + 1
+    slides[currentSlide].classList.add("current");
+    return currentSlide
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     const navElms = document.querySelectorAll("nav > ul > li");
     const contentElms = document.querySelectorAll("main > section");
     const nameElm = document.getElementById("name");
     const titleElm = document.getElementById("title");
     const detailsElm = document.getElementById("details");
-    if(navElms.length != contentElms.length){
+    const slider = document.getElementById("prev-work");
+    if (navElms.length != contentElms.length) {
         console.log("Navigation error");
         return
     }
 
-    navElms.forEach((elm,i)=>{
-        scroll(elm,contentElms[i])
+    navElms.forEach((elm, i) => {
+        scroll(elm, contentElms[i])
     })
-    const  name = "Riad Adel";
-    const  title = "Computer Engineering Student";
-    const  details = "I love to work as a front-End developer";
+    const name = "Riad Adel";
+    const title = "Computer Engineering Student";
+    const details = "I love to work as a front-End developer";
     const delay = 150;
 
-    typingAnimation(nameElm,name,delay)
-    setTimeout(()=>typingAnimation(titleElm,title,delay),delay*name.length);
-    setTimeout(()=>typingAnimation(detailsElm,details,delay,false),delay*name.length + delay*title.length);    
+    typingAnimation(nameElm, name, delay)
+    setTimeout(() => typingAnimation(titleElm, title, delay), delay * name.length);
+    setTimeout(() => typingAnimation(detailsElm, details, delay, false), delay * name.length + delay * title.length);
+
+    const leftArrow = slider.querySelector("#left");
+    const rightArrow = slider.querySelector("#right");
+    let currentSlide = 0;
+    leftArrow.addEventListener('click', () => {
+        currentSlide = prevSlide(slider, currentSlide)
+    });
+    rightArrow.addEventListener('click', () => {
+        currentSlide = nextSlide(slider, currentSlide)
+    });
+
 })
