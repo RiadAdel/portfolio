996541db1c98dd860d895c173931d953aadd7f66
Riad Adel <riadadel22@gmail.com>
2020-11-06 22:36:48 +0200

    responsive header

--- scripts/index.js
+++ scripts/index.js
@@ -1,77 +1,74 @@
 function scroll(trigger, content) {
-    trigger.addEventListener('click', function (e) {
-        console.log(e)
-        content.scrollIntoView(false);
-    })
+  trigger.addEventListener("click", function (e) {
+    console.log(e);
+    content.scrollIntoView(false);
+  });
 }
 
-function typingAnimation(elm, content, delay = 150, removeAfterFinish = true) {
-    let count = 0;
-    elm.classList.add("cursor")
-    let intervalID = setInterval(function () {
-        elm.innerText += content[count++];
-        if (count == content.length) {
-            clearInterval(intervalID)
-            if (removeAfterFinish)
-                elm.classList.remove("cursor")
-        }
-    }, delay)
+function typingAnimation(elm, content, delay = 100, removeAfterFinish = true) {
+  let count = 0;
+  elm.classList.add("cursor");
+  let intervalID = setInterval(function () {
+    elm.innerText += content[count++];
+    if (count == content.length) {
+      clearInterval(intervalID);
+      if (removeAfterFinish) elm.classList.remove("cursor");
+    }
+  }, delay);
 }
 
 function prevSlide(slider, currentSlide) {
-    const slides = slider.querySelectorAll("div.slide");
-    slides[currentSlide].classList.remove("current"); 
-    currentSlide = currentSlide == 0 ? slides.length - 1 : currentSlide - 1
-    slides[currentSlide].classList.add("current");
-    return currentSlide
+  const slides = slider.querySelectorAll("div.slide");
+  slides[currentSlide].classList.remove("current");
+  currentSlide = currentSlide == 0 ? slides.length - 1 : currentSlide - 1;
+  slides[currentSlide].classList.add("current");
+  return currentSlide;
 }
 
 function nextSlide(slider, currentSlide) {
-    const slides = slider.querySelectorAll("div.slide");
-    slides[currentSlide].classList.remove("current");
-    currentSlide = currentSlide == slides.length - 1 ? 0 : currentSlide + 1
-    slides[currentSlide].classList.add("current");
-    return currentSlide
+  const slides = slider.querySelectorAll("div.slide");
+  slides[currentSlide].classList.remove("current");
+  currentSlide = currentSlide == slides.length - 1 ? 0 : currentSlide + 1;
+  slides[currentSlide].classList.add("current");
+  return currentSlide;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const navElms = document.querySelectorAll("nav > ul > li");
-    const contentElms = document.querySelectorAll("main > section");
-    const nameElm = document.getElementById("name");
-    const titleElm = document.getElementById("title");
-    const detailsElm = document.getElementById("details");
-    const slider = document.getElementById("prev-work");
-    const sendEmailFormElm = document.getElementById("send-email")
-    if (navElms.length != contentElms.length) {
-        console.log("Navigation error");
-        return
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const navElms = document.querySelectorAll("nav > ul > li");
+  const contentElms = document.querySelectorAll("main > section");
+  const detailsElm = document.getElementById("details");
+  const slider = document.getElementById("prev-work");
+  const sendEmailFormElm = document.getElementById("send-email");
+  if (navElms.length != contentElms.length) {
+    console.log("Navigation error");
+    return;
+  }
 
-    navElms.forEach((elm, i) => {
-        scroll(elm, contentElms[i])
-    })
-    const name = "Riad Adel";
-    const title = "Computer Engineer";
-    const details = "I am focusing on learning frontend frameworks such as React and React Native.";
-    const delay = 150;
+  navElms.forEach((elm, i) => {
+    scroll(elm, contentElms[i]);
+  });
 
-    typingAnimation(nameElm, name, delay)
-    setTimeout(() => typingAnimation(titleElm, title, delay), delay * name.length);
-    setTimeout(() => typingAnimation(detailsElm, details, delay, false), delay * name.length + delay * title.length);
+  const details =
+    "Hi there! 😁\n I am a Frontend Developer and a Javascript Lover 👌";
+  const delay = 85;
 
-    const leftArrow = slider.querySelector("#left");
-    const rightArrow = slider.querySelector("#right");
-    let currentSlide = 0;
-    leftArrow.addEventListener('click', () => {
-        currentSlide = prevSlide(slider, currentSlide)
-    });
-    rightArrow.addEventListener('click', () => {
-        currentSlide = nextSlide(slider, currentSlide)
-    });
+  typingAnimation(detailsElm, details, delay, false);
 
-    sendEmailFormElm.addEventListener("submit",function(e){
-        e.preventDefault();
-        const data = Object.fromEntries(new FormData(sendEmailFormElm).entries());
-        window.open(`mailto:riadadel22@gmail.com?subject=${data.subject}&body=${data.content}`)
-    })
-})
+  const leftArrow = slider.querySelector("#left");
+  const rightArrow = slider.querySelector("#right");
+  let currentSlide = 0;
+  leftArrow.addEventListener("click", () => {
+    currentSlide = prevSlide(slider, currentSlide);
+  });
+  rightArrow.addEventListener("click", () => {
+    currentSlide = nextSlide(slider, currentSlide);
+  });
+
+  sendEmailFormElm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(sendEmailFormElm).entries());
+    window.open(
+      `mailto:riadadel22@gmail.com?subject=${data.subject}&body=${data.content}`
+    );
+  });
+});
